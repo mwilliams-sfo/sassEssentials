e1d88e6d12414ef032a5d9ee7c2fbcadc443f2ac
Myles Williams <mwilliams.sfo@gmail.com>
2020-09-23 21:15:18 -0700

    Upgrade to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     webserver = require('gulp-webserver');
 
@@ -11,20 +11,22 @@ gulp.task('js', function() {
 });
 
 gulp.task('sass', function () {
-    return sass('process/sass/style.scss', {
-      sourcemap: true,
-      style: 'expanded'
-    })
-    .on('error', function (err) {
-        console.error('Error!', err.message);
-    })
+    return gulp.src('process/sass/style.scss')
+      .pipe(
+        sass({
+          sourceMap: true,
+          outputStyle: 'expanded'
+        })
+        .on('error', function (err) {
+          console.error('Error!', err.message);
+        }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('builds/sassEssentials/css'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('builds/sassEssentials/js/**/*', ['js']);
-  gulp.watch(['process/sass/**/*'], ['sass']);
+  gulp.watch('builds/sassEssentials/js/**/*', gulp.series('js'));
+  gulp.watch(['process/sass/**/*'], gulp.series('sass'));
 });
 
 gulp.task('webserver', function() {
@@ -35,4 +37,6 @@ gulp.task('webserver', function() {
         }));
 });
 
-gulp.task('default', ['watch', 'sass','webserver']);
+gulp.task('run', gulp.parallel('watch', 'webserver'));
+
+gulp.task('default', gulp.series('sass', 'run'));
